bf13937bcb79be4f2006065f5d1de1bc0954b6dd
Claus Bove <clausbove@gmail.com>
2018-09-03 11:39:20 +0200

    dat18 line added from clbo

--- Main.java
+++ Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hello GIT!");
+        System.out.println("Hello Dat18v2");
     }
 }
